35d2fc34a369977e4edb2ae3234bfb07b1bb7bcd
Snourry <30350968+Existrace@users.noreply.github.com>
2023-06-13 17:27:48 +0200

    Refacto command

--- ordersproject/orders/management/commands/importdatatoxml.py
+++ ordersproject/orders/management/commands/importdatatoxml.py
@@ -14,39 +14,53 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Retrieve the XML file path from command-line arguments
         xml_file_path = options.get("xml_file")
+
         try:
             # Parse the XML file
             tree = ET.parse(xml_file_path)
             root = tree.getroot()
-        except Exception:
+        except ET.ParseError:
             # Handle invalid XML file error
             self.stderr.write(self.style.ERROR("Invalid XML file."))
+            return
 
-        try:
-            # Process each order element
-            for order in root.findall("orders/order"):
-                # Extract field values from the order element
-                order_id = order.find("order_id").text
-                marketplace = order.find("marketplace").text
-                order_purchase_date = order.find("order_purchase_date").text
-                order_currency = order.find("order_currency").text
-                order_amount = order.find("order_amount").text
+        for order in root.findall("orders/order"):
+            # Extract field values from the order element
+            order_id = order.findtext("order_id")
+            marketplace = order.findtext("marketplace")
+            order_purchase_date = order.findtext("order_purchase_date")
+            order_currency = order.findtext("order_currency")
+            order_amount = order.findtext("order_amount")
 
-                # Now need to create an order object
-                order = Order.objects.create(
-                    order_id=order_id,
-                    marketplace=marketplace,
-                    order_purchase_date=order_purchase_date,
-                    order_currency=order_currency,
-                    order_amount=order_amount,
+            if None in (
+                order_id,
+                marketplace,
+                order_purchase_date,
+                order_currency,
+                order_amount,
+            ):
+                # Handle missing elements error
+                self.stderr.write(
+                    self.style.ERROR("Some elements are missing in the XML.")
                 )
+                continue
 
-                order.save()
+            # Create or retrieve the Order object
+            order_obj, created = Order.objects.get_or_create(
+                order_id=order_id,
+                defaults={
+                    "marketplace": marketplace,
+                    "order_purchase_date": order_purchase_date,
+                    "order_currency": order_currency,
+                    "order_amount": order_amount,
+                },
+            )
 
-                self.stdout.write(f"Order saved : {order}")
-
-            # Indicate successful processing
-            self.stdout.write(self.style.SUCCESS("Successfully saved data."))
-        except Exception as e:
-            # Handle invalid XML file error
-            self.stderr.write(self.style.ERROR(f"Error : {e}"))
+            if created:
+                self.stdout.write(f"Order created: {order_obj}")
+                # Indicate successful processing
+                self.stdout.write(self.style.SUCCESS("Data saved."))
+            else:
+                self.stdout.write(
+                    self.style.ERROR(f"Order already exists: {order_obj}")
+                )
