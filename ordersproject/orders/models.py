d32256f330f6e251efd29b9c705e7dad02f5d75e
Snourry <30350968+Existrace@users.noreply.github.com>
2023-06-13 10:42:45 +0200

    create order model

--- ordersproject/orders/models.py
+++ ordersproject/orders/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    marketplace = models.CharField(max_length=100)
+    idFlux = models.CharField(max_length=100)
+    order_status_marketplace = models.CharField(max_length=100)
+    order_status_lengow = models.CharField(max_length=100)
+    order_id = models.CharField(max_length=100)
