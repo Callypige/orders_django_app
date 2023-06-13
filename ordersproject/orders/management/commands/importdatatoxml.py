a01a7e01eae0fb38ae0847e80f4e3a8ef808f267
Snourry <30350968+Existrace@users.noreply.github.com>
2023-06-13 15:07:51 +0200

    Create order in command + change order model

--- ordersproject/orders/management/commands/importdatatoxml.py
+++ ordersproject/orders/management/commands/importdatatoxml.py
@@ -1,29 +1,38 @@
 from django.core.management.base import BaseCommand, CommandParser
+from orders.models import Order
 import xml.etree.ElementTree as ET
 
 
 class Command(BaseCommand):
+    # Command help description
     help = "Imports order from XML file to the order model"
 
     def add_arguments(self, parser: CommandParser) -> None:
+        # Add command-line argument for XML file path
         parser.add_argument("xml_file", type=str, help="Path to the XML file")
 
     def handle(self, *args, **options):
+        # Retrieve the XML file path from command-line arguments
         xml_file_path = options.get("xml_file")
         try:
+            # Parse the XML file
             tree = ET.parse(xml_file_path)
             root = tree.getroot()
+        except Exception:
+            # Handle invalid XML file error
+            self.stderr.write(self.style.ERROR("Invalid XML file."))
 
-            # count_total = int(tree.find("orders_count/count_total").text)
-
+        try:
+            # Process each order element
             for order in root.findall("orders/order"):
+                # Extract field values from the order element
                 order_id = order.find("order_id").text
                 marketplace = order.find("marketplace").text
                 order_purchase_date = order.find("order_purchase_date").text
                 order_currency = order.find("order_currency").text
                 order_amount = order.find("order_amount").text
 
-                # Afficher les valeurs des champs
+                # Display field values
                 print("order_id =", order_id)
                 print("marketplace =", marketplace)
                 print("order_purchase_date =", order_purchase_date)
@@ -31,6 +40,21 @@ class Command(BaseCommand):
                 print("order_amount =", order_amount)
                 print()
 
-            self.stdout.write(self.style.SUCCESS("Successfull"))
-        except Exception:
-            self.stderr.write(self.style.ERROR("Invalid XML file."))
+                # Now need to create an order object
+                order = Order.objects.create(
+                    order_id=order_id,
+                    marketplace=marketplace,
+                    order_purchase_date=order_purchase_date,
+                    order_currency=order_currency,
+                    order_amount=order_amount,
+                )
+
+                order.save()
+
+            # Indicate successful processing
+            self.stdout.write(
+                self.style.SUCCESS("Successfully processed the XML file.")
+            )
+        except Exception as e:
+            # Handle invalid XML file error
+            self.stderr.write(self.style.ERROR(f"Error : {e}"))
--- ordersproject/orders/models.py
+++ ordersproject/orders/models.py
@@ -5,6 +5,10 @@ import datetime
 class Order(models.Model):
     order_id = models.CharField(max_length=100)
     marketplace = models.CharField(max_length=100)
-    order_purchase_date = models.DateField(default=datetime.date.today)
+    order_purchase_date = models.DateField(
+        default=datetime.date.today, null=True
+    )
     order_currency = models.CharField(max_length=10, default="EUR")
-    order_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    order_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0
+    )
